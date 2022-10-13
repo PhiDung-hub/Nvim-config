8bfff7e44215695d8896fff5a9cb55dbc51f3621
Phi <dungho01092002@gmail.com>
2022-10-13 14:24:22 +0800

    add aerial

---
+++ after/plugin/editor_utils/aerial.rc.lua
@@ -0,0 +1,185 @@
+local status, aerial = pcall(require, "aerial")
+if (not status) then return end
+
+aerial.setup {
+  backends = { "treesitter", "lsp", "markdown" },
+
+  layout = {
+    default_direction = "left",
+    placement = "window",
+    min_width = { 30, .1 },
+    max_width = { 60, .2 },
+    width = nil,
+  },
+
+  -- Call this function when aerial attaches to a buffer.
+  -- Useful for setting keymaps. Takes a single `bufnr` argument.
+  on_attach = function(bufnr)
+    -- Toggle the aerial window with <leader>a
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>a', '<cmd>AerialToggle!<CR>', {})
+    -- Jump forwards/backwards with '{' and '}'
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>[', '<cmd>AerialPrev<CR>', {})
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>]', '<cmd>AerialNext<CR>', {})
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '2<leader>[', '<cmd>AerialPrev 2<CR>', {})
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '2<leader>]', '<cmd>AerialNext 2<CR>', {})
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '3<leader>[', '<cmd>AerialPrev 3<CR>', {})
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '3<leader>]', '<cmd>AerialNext 3<CR>', {})
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '4<leader>[', '<cmd>AerialPrev 4<CR>', {})
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '4<leader>]', '<cmd>AerialNext 4<CR>', {})
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '5<leader>[', '<cmd>AerialPrev 5<CR>', {})
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '5<leader>]', '<cmd>AerialNext 5<CR>', {})
+    -- -- Jump up the tree with '[[' or ']]'
+    -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '[[', '<cmd>AerialPrevUp<CR>', {})
+    -- vim.api.nvim_buf_set_keymap(bufnr, 'n', ']]', '<cmd>AerialNextUp<CR>', {})
+  end,
+
+  -- Determines how the aerial window decides which buffer to display symbols for
+  --   window - aerial window will display symbols for the buffer in the window from which it was opened
+  --   global - aerial window will display symbols for the current window
+  attach_mode = "window",
+
+  -- List of enum values that configure when to auto-close the aerial window
+  --   unfocus       - close aerial when you leave the original source window
+  --   switch_buffer - close aerial when you change buffers in the source window
+  --   unsupported   - close aerial when attaching to a buffer that has no symbol source
+  close_automatic_events = {
+    "unfocus", "unsupported",
+  },
+
+  -- Set to false to remove the default keybindings for the aerial buffer
+  default_bindings = true,
+
+  -- A list of all symbols to display. Set to false to display all symbols.
+  -- This can be a filetype map (see :help aerial-filetype-map)
+  -- To see all available values, see :help SymbolKind
+  filter_kind = {
+    "Class",
+    "Constant",
+    "Constructor",
+    "Enum",
+    "Function",
+    "Interface",
+    "Method",
+    "Module",
+    "Object",
+    "Struct",
+  },
+
+  -- Determines line highlighting mode when multiple splits are visible.
+  -- split_width   Each open window will have its cursor location marked in the
+  --               aerial buffer. Each line will only be partially highlighted
+  --               to indicate which window is at that location.
+  -- full_width    Each open window will have its cursor location marked as a
+  --               full-width highlight in the aerial buffer.
+  -- last          Only the most-recently focused window will have its location
+  --               marked in the aerial buffer.
+  -- none          Do not show the cursor locations in the aerial window.
+  highlight_mode = "split_width",
+
+  -- Highlight the closest symbol if the cursor is not exactly on one.
+  highlight_closest = true,
+
+  -- Highlight the symbol in the source buffer when cursor is in the aerial win
+  highlight_on_hover = false,
+
+  -- When jumping to a symbol, highlight the line for this many ms.
+  -- Set to false to disable
+  highlight_on_jump = 300,
+
+  -- When you fold code with za, zo, or zc, update the aerial tree as well.
+  -- Only works when manage_folds = true
+  link_folds_to_tree = false,
+
+  -- Fold code when you open/collapse symbols in the tree.
+  -- Only works when manage_folds = true
+  link_tree_to_folds = true,
+
+  -- Use symbol tree for folding. Set to true or false to enable/disable
+  -- 'auto' will manage folds if your previous foldmethod was 'manual'
+  manage_folds = false,
+
+  -- Set default symbol icons to use patched font icons (see https://www.nerdfonts.com/)
+  -- "auto" will set it to true if nvim-web-devicons or lspkind-nvim is installed.
+  nerd_font = "auto",
+
+  -- Call this function when aerial first sets symbols on a buffer.
+  -- Takes a single `bufnr` argument.
+  on_first_symbols = nil,
+
+  -- Automatically open aerial when entering supported buffers.
+  -- This can be a function (see :help aerial-open-automatic)
+  open_automatic = false,
+
+  -- Run this command after jumping to a symbol (false will disable)
+  post_jump_cmd = "normal! zz",
+
+  -- When true, aerial will automatically close after jumping to a symbol
+  close_on_select = false,
+
+  -- Show box drawing characters for the tree hierarchy
+  show_guides = false,
+
+  -- The autocmds that trigger symbols update (not used for LSP backend)
+  update_events = "TextChanged,InsertLeave",
+
+  -- Customize the characters used when show_guides = true
+  guides = {
+    -- When the child item has a sibling below it
+    mid_item = "├─",
+    -- When the child item is the last in the list
+    last_item = "└─",
+    -- When there are nested child guides to the right
+    nested_top = "│ ",
+    -- Raw indentation
+    whitespace = "  ",
+  },
+
+  -- Options for opening aerial in a floating win
+  float = {
+    -- Controls border appearance. Passed to nvim_open_win
+    border = "rounded",
+
+    -- Determines location of floating window
+    --   cursor - Opens float on top of the cursor
+    --   editor - Opens float centered in the editor
+    --   win    - Opens float centered in the window
+    relative = "editor",
+
+    -- These control the height of the floating window.
+    -- They can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+    -- min_height and max_height can be a list of mixed types.
+    -- min_height = {8, 0.1} means "the greater of 8 rows or 10% of total"
+    max_height = 0.9,
+    height = nil,
+    min_height = { 8, 0.1 },
+
+    override = function(conf, source_winid)
+      -- This is the config that will be passed to nvim_open_win.
+      -- Change values here to customize the layout
+      return conf
+    end,
+  },
+
+  lsp = {
+    -- Fetch document symbols when LSP diagnostics update.
+    -- If false, will update on buffer changes.
+    diagnostics_trigger_update = true,
+
+    -- Set to false to not update the symbols when there are LSP errors
+    update_when_errors = true,
+
+    -- How long to wait (in ms) after a buffer change before updating
+    -- Only used when diagnostics_trigger_update = false
+    update_delay = 300,
+  },
+
+  treesitter = {
+    -- How long to wait (in ms) after a buffer change before updating
+    update_delay = 300,
+  },
+
+  markdown = {
+    -- How long to wait (in ms) after a buffer change before updating
+    update_delay = 300,
+  },
+}
